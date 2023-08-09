5aae882f32ec5a3c4dc3ce8abb768b8ebb024a41
KellyMark <kmark602@gmail.com>
2023-08-09 15:41:43 -0400

    commit tree 949d14f34cf8459996d7cc1a1e058358cb81027d

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,7 +8,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -18,9 +18,30 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant, except: [:created_at, :updated_at]
+  end
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def find_plant
+    Plant.find(params[:id])
+  end
+
+  def plant_not_found
+    render json: {error: "Plant not found"}, status: :plant_not_found
+  end
+
 end
